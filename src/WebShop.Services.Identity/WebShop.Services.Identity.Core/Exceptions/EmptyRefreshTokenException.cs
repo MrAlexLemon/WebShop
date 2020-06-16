9e4acaafe4f49aad75529dd2213355ea40b16ae0
Alex <lileynuk@gmail.com>
2020-06-16 15:21:55 +0300

    Added refreshToken

---
+++ src/WebShop.Services.Identity/WebShop.Services.Identity.Core/Exceptions/EmptyRefreshTokenException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop.Services.Identity.Core.Exceptions
+{
+    public class EmptyRefreshTokenException : DomainException
+    {
+        public override string Code { get; } = "empty_refresh_token";
+
+        public EmptyRefreshTokenException() : base("Empty refresh token.")
+        {
+        }
+    }
+}
---
+++ src/WebShop.Services.Identity/WebShop.Services.Identity.Core/Exceptions/RevokedRefreshTokenException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop.Services.Identity.Core.Exceptions
+{
+    public class RevokedRefreshTokenException : DomainException
+    {
+        public override string Code { get; } = "revoked_refresh_token";
+
+        public RevokedRefreshTokenException() : base("Revoked refresh token.")
+        {
+        }
+    }
+}
