752fe1c0635be885391e3c4f487a0bcf7caa0ead
Alex <lileynuk@gmail.com>
2020-07-01 23:30:45 +0300

    Added base entities and exceptions.

---
+++ src/WebShop.Services.Products/WebShop.Services.Products.Core/Entities/Base/IIdentifiable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop.Services.Products.Core.Entities.Base
+{
+    public interface IIdentifiable
+    {
+        Guid Id { get; }
+    }
+}
