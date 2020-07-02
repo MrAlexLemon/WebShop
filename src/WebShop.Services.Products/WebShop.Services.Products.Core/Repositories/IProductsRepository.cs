f53cc2462b4cd444b4b6e0d9c921462f0d0d8915
Alex <lileynuk@gmail.com>
2020-07-02 10:43:13 +0300

    Added interface repositories for mongodb

---
+++ src/WebShop.Services.Products/WebShop.Services.Products.Core/Repositories/IProductsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.Services.Products.Core.Entities;
+using WebShop.Services.Products.Core.Utils;
+
+namespace WebShop.Services.Products.Core.Repositories
+{
+    public interface IProductsRepository
+    {
+        Task<Product> GetAsync(Guid id);
+        Task<bool> ExistsAsync(Guid id);
+        Task<bool> ExistsAsync(string name);
+        Task<PagedResult<Product>> BrowseAsync(BrowseProducts query);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(Guid id);
+    }
+}
---
+++ src/WebShop.Services.Products/WebShop.Services.Products.Core/Utils/BrowseProducts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop.Services.Products.Core.Utils
+{
+    public class BrowseProducts : PagedQueryBase
+    {
+        public decimal PriceFrom { get; set; }
+        public decimal PriceTo { get; set; } = decimal.MaxValue;
+    }
+}
